d4de03abf68934683422f12922b88982063d0683
집_twoo0220 <twoo0220@gmail.com>
2022-05-08 23:33:14 +0900

    3d glm math 추가

--- OpenGL/default.vert
+++ OpenGL/default.vert
@@ -17,9 +17,14 @@ out vec2 texCoord;
 // Controls the scale of the vertices
 uniform float scale;
 
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 proj;
+
 void main()
 {
-	gl_Position = vec4(aPos.x + aPos.x * scale, aPos.y + aPos.y * scale, aPos.z + aPos.z * scale, 1.0);
+	//gl_Position = vec4(aPos.x + aPos.x * scale, aPos.y + aPos.y * scale, aPos.z + aPos.z * scale, 1.0);
+	gl_Position = proj * view * model * vec4(aPos, 1.0);
 	color = aColor;
 	texCoord = aTex;
 }
--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -12,19 +12,28 @@
 #include "VBO.h"
 #include "EBO.h"
 
+const unsigned int width = 800;
+const unsigned int height = 800;
+
 // 일반 부동소수점을 사용할 수도 있지만, opengl의 부동소수점은 다를 수도 있으므로 OpenGL 버전을 사용하는 것이 더 안전함
 GLfloat vertices[] =
 { //	COORDINATES		 /			COLORS
-	-0.5f, -0.5f, 0.0f,		0.8f,	0.3f,	0.02f, 	 0.0f, 0.0f, // Lower left corner
-	-0.5f,  0.5f, 0.0f,		0.8f,	0.3f,	0.02f,	 0.0f, 1.0f,	// Lower righr corner
-	 0.5f,	0.5f, 0.0f,		1.0f,	0.6f,	0.32f,	 1.0f, 1.0f,	// Upper corner
-	 0.5f, -0.5f, 0.0f,		0.9f,	0.45f,	0.17f,	 1.0f, 0.0f	// Inner left
+	-0.5f, 0.0f, 0.5f,		0.83f,	0.3f,	0.02f, 	 0.0f, 0.0f, // Lower left corner
+	-0.5f, 0.0f, -0.5f,		0.83f,	0.3f,	0.02f,	 5.0f, 0.0f,	// Lower righr corner
+	 0.5f, 0.0f, -0.5f,		0.83f,	0.6f,	0.32f,	 0.0f, 0.0f,	// Upper corner
+	 0.5f, 0.0f, 0.5f,		0.9f,	0.45f,	0.17f,	 5.0f, 0.0f,	// Inner left
+	 0.0f, 0.8f, 0.0f,		0.9f,	0.45f,	0.17f,	 2.5f, 5.0f	// Inner left
+
 };
 
 GLuint indices[] =
 {
-	0, 2, 1, // Lower left triangle
-	0, 3, 2 // Lower right triangle
+	0, 1, 2, // Lower left triangle
+	0, 2, 3, // Lower right triangle
+	0, 1, 4, // Lower right triangle
+	1, 2, 4, // Lower right triangle
+	2, 3, 4, // Lower right triangle
+	3, 0, 4 // Lower right triangle
 };
 
 
@@ -44,7 +53,7 @@ int main()
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Create a GLFWwindow object of 800 by 800 pixels, naming it "OpenGL"
-	GLFWwindow* window = glfwCreateWindow(800, 800, "OpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(width, height, "OpenGL", NULL, NULL);
 
 	// Error check if the window fails to create
 	if (window == NULL)
@@ -62,7 +71,7 @@ int main()
 
 	// Specify the viewport of OpenGL in the Window
 	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
-	glViewport(0, 0, 800, 800);
+	glViewport(0, 0, width, height);
 
 	Shader shaderProgram("default.vert", "default.frag");
 
@@ -107,18 +116,47 @@ int main()
 	Texture popCat("pop_cat.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
 	popCat.texUnit(shaderProgram, "tex0", 0);
 
+	float rotation = 0.0f;
+	double prevTime = glfwGetTime();
+
+	glEnable(GL_DEPTH_TEST);
+
 	// Main while loop
 	while (!glfwWindowShouldClose(window))
 	{
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		shaderProgram.Activate();
+
+		double curTime = glfwGetTime();
+		if (curTime - prevTime >= (1 / 60))
+		{
+			rotation += 0.5f;
+			prevTime = curTime;
+		}
+
+		glm::mat4 model = glm::mat4(1.0f);
+		glm::mat4 view = glm::mat4(1.0f);
+		glm::mat4 proj = glm::mat4(1.0f);
+
+		model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f));
+		view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
+		proj = glm::perspective(glm::radians(45.0f), (float)(width / height), 0.1f, 100.0f);
+
+		int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		int viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		int projLoc = glGetUniformLocation(shaderProgram.ID, "proj");
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
+
+
 		glUniform1f(uniID, 0.5f); // 쉐이더활성화 이후에 실행해야함
 		popCat.Bind();
 
 		vao1.Bind();
 		
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
 
 		// Take care of all GLFW events
