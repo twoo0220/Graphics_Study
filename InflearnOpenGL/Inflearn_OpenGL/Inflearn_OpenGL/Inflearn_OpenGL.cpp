2820dbd8e726d82eac756b2b1d15b5e10ae51dfc
twoo0220 <twoo0220@gmail.com>
2024-09-16 19:19:07 +0900

    CMake setup

---
+++ InflearnOpenGL/Inflearn_OpenGL/Inflearn_OpenGL/Inflearn_OpenGL.cpp
@@ -0,0 +1,34 @@
+﻿#include "Inflearn_OpenGL.h"
+#include "GLFW/glfw3.h"
+
+int main()
+{
+	std::cout << "Hello CMake.";
+
+	GLFWwindow* window;
+
+	if (!glfwInit())
+	{
+		std::cerr << "glfwInit Error";
+		return -1;
+	}
+
+	window = glfwCreateWindow(640, 480, "twoo0220", NULL, NULL);
+	if (window == nullptr)
+	{
+		glfwTerminate();
+		std::cerr << "glfwCreatWindow failed\n";
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
