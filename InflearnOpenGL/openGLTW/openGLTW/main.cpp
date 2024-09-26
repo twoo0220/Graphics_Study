6efa3d80a4b2cd05a77ef0e542f86307f1135422
twoo0220 <twoo0220@gmail.com>
2024-09-27 01:33:59 +0900

    glfw set up

--- InflearnOpenGL/openGLTW/openGLTW/main.cpp
+++ InflearnOpenGL/openGLTW/openGLTW/main.cpp
@@ -1,6 +1,37 @@
-#include "Renderer.h"
+#include "GLFW/glfw3.h"
 
 int main()
 {
+	GLFWwindow* window;
+
+	if (!glfwInit())
+	{
+		return -1;
+	}
+
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+		glBegin(GL_TRIANGLES);
+		glColor3f(1.0f, 0.0f, 0.0f);
+		glVertex2f(-0.5f, 0.0f);
+		glVertex2f(0.0f, 0.5f);
+		glVertex2f(0.5f, 0.0f);
+		glEnd();
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
 }
