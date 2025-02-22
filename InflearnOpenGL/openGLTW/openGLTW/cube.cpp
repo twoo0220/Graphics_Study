176bc79731bc65bcc673e11385ded01e8f1971cd
twoo0220 <twoo0220@gmail.com>
2025-02-22 18:19:56 +0900

    25.02.22 - cube update

--- InflearnOpenGL/openGLTW/openGLTW/cube.cpp
+++ InflearnOpenGL/openGLTW/openGLTW/cube.cpp
@@ -2,10 +2,25 @@
 
 void cube::initialize()
 {
+	glGenVertexArrays(1, &mVAO);
+	glGenBuffers(1, &mVBO);
+
+	glBindVertexArray(mVAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, mVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(mVertCube), mVertCube, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), static_cast<void*>(0));
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 }
 
 void cube::deinitialize()
 {
+	glDeleteVertexArrays(1, &mVAO);
+	glDeleteBuffers(1, &mVBO);
 }
 
 void cube::update()
